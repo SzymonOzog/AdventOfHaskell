2572a59ddc4b3ef4ae7ad2190d0332a52d656d09
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2023-12-06 18:32:49 +0100

    Day 6 part 1

---
+++ day6.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE ParallelListComp #-}
+import Data.Char
+
+rInt :: String -> Int
+rInt = read
+
+findWinningTimes :: (Int,Int) -> Int
+findWinningTimes (duration, best) = high - low + 1
+    where
+        delta = fromIntegral ((duration^2) - (4*best))
+        eps = 1e-10
+        low = ceiling (((fromIntegral duration - sqrt delta) / 2) + eps)
+        high = floor (((fromIntegral duration + sqrt delta) / 2) - eps)
+
+main :: IO ()
+main = do
+    contents <- readFile "day6.txt"
+    print contents
+    let timeLine = head (lines contents)
+    let distanceLine = last (lines contents)
+
+    let times = map rInt (words (drop 6 timeLine))
+    let distances = map rInt (words (drop 11 distanceLine))
+    let timeToDistance = [(t,d) | t <- times| d<- distances]
+    print (product (map findWinningTimes timeToDistance))
