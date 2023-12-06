e739f947473a3a5e6e7eab2ad13575ef3145dd7b
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2023-12-06 09:39:49 +0100

    Day5 part 2
    this was fun

--- day5.hs
+++ day5.hs
@@ -1,4 +1,6 @@
 import Data.Char
+import Data.Function
+import Data.List
 
 getMap :: [String] -> [(Int, Int, Int)]
 getMap mapLines = map (\x -> (rInt (x!!0), rInt (x!!1),rInt (x!!2) )) w
@@ -9,17 +11,56 @@ splitList :: [String] -> (String->Bool)-> [[String]]
 splitList [] _ = []
 splitList l pred = takeWhile pred l : splitList (drop 1 (dropWhile pred l)) pred
 
-getLocation :: Int -> [[(Int, Int, Int)]] -> Int
-getLocation = foldl getIdentifier
-
-getIdentifier :: Int -> [(Int, Int, Int)] -> Int
-getIdentifier key [] = key
-getIdentifier key ((a,b,c):xs)
-    | keyInRange = mappedKey
-    | otherwise = getIdentifier key xs
+getLocation :: [(Int,Int)] -> [[(Int, Int, Int)]] -> Int
+getLocation range maps = fst (minimumBy (compare `on` fst) outRanges)
     where
-        keyInRange = key>=b && key < b+c
-        mappedKey = a + key - b
+        outRanges = foldl getIdentifier range maps
+
+extendSeeds :: [Int] -> [(Int,Int)]
+extendSeeds [] = []
+extendSeeds (x:x2:xs) = (x,x2) : extendSeeds xs
+
+getIdentifier :: [(Int,Int)] -> [(Int, Int, Int)] -> [(Int,Int)]
+getIdentifier range [] = range
+getIdentifier [] _ = []
+getIdentifier (x2:xs2) (x:xs) = next  ++ getIdentifier left xs ++ getIdentifier right xs ++ getIdentifier xs2 (x:xs)
+    where
+        next = getNextRange x2 x
+        left = getLeftRemainder x2 x
+        right = getRightRemainder x2 x
+
+getNextRange :: (Int, Int) -> (Int,Int, Int) -> [(Int,Int)]
+getNextRange (start, length) (a,b,c)
+    | isInRange = [(a, c)]
+    | isInRangeM = [(a+start-b, length)]
+    | isInRangeL = [(a, length-(b-start))]
+    | isInRangeR = [(a+start-b, c-(start-b))]
+    | otherwise =  []
+    where
+        isInRange = (start<=b) && ((start+length)>=(b+c))
+        isInRangeM = (start>=b) && ((start+length)<=(b+c))
+        isInRangeL = ((start+length)>b) && (start<b)
+        isInRangeR = (start>b) && (start<(b+c))
+
+getLeftRemainder :: (Int, Int) -> (Int,Int, Int) -> [(Int,Int)]
+getLeftRemainder (start, length) (a,b,c)
+    | isOutOfRange = [(start,length)]
+    | isInRangeL || isInRange = [(start,b-start)]
+    | otherwise = []
+    where
+        isInRange = (start<=b) && ((start+length)>(b+c)) && (b-start)>0
+        isInRangeL = start<b
+        isOutOfRange = (start+length) < b
+
+getRightRemainder :: (Int, Int) -> (Int,Int, Int) -> [(Int,Int)]
+getRightRemainder (start, length) (a,b,c)
+    | isOutOfRange = [(start,length)]
+    | isInRangeR || isInRange = [(c+b, (start+length)-(c+b))]
+    | otherwise = []
+    where
+        isInRangeR = (start+length) > (b+c) -- && (start<(b+c))
+        isInRange = (start<=b) && ((start+length)>(b+c))
+        isOutOfRange = start > (b+c)
 
 rInt :: String -> Int
 rInt = read
@@ -30,4 +71,8 @@ main = do
     let seeds = map rInt (drop 1 (words (head contentLines)))
     let all_maps = filter (/="") (drop 3 contentLines)
     let filteredMaps = map getMap (splitList all_maps (isDigit . head))
-    print (minimum ( map (`getLocation` filteredMaps) seeds))
+    let initialRanges = [(x,1)| x<-seeds]
+    print (getLocation initialRanges filteredMaps)
+
+    let extendedSeeds = extendSeeds seeds
+    print (getLocation extendedSeeds filteredMaps)
